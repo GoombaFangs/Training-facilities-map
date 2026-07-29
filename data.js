571bf12004f34715342f9e5eaaef2ed8c85f69a2
Goomba <goombafangs@gmail.com>
2026-07-29 12:32:04 +0300

    add project

---
+++ data.js
@@ -0,0 +1,18 @@
+let data = [
+    {
+        nameOfFacility: '',
+        unitOwningTheFacility: '',
+        coordinates: [0, 0],
+        areaInTheCountry: 'צפון, מרכז, דרום',
+
+        TypesOfFacilities: [
+            {
+                typeOfFacility: '',
+                trainingOptions: [],
+                TrainingFrame: '',
+                imgArr: [],
+                comments: ''
+            },
+        ]
+    },
+];
