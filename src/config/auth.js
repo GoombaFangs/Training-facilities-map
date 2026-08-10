1f1dc86732becf6626606e9d0a9d604696283ff2
Goomba <goombafangs@gmail.com>
2026-08-10 14:26:22 +0300

    +1

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,7 +1,7 @@
 /** Admin authentication settings */
 
-export const ADMIN_USERNAME = 'ראשי';
-export const ADMIN_PASSWORD = '123';
+export const ADMIN_USERNAME = 'admin';
+export const ADMIN_PASSWORD = '123456';
 
 export const ROLES = {
   GUEST: 'guest',
